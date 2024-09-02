aa722ea4a569430d50f2f2ffbd381f0924ff5e87
miguel <miguelcalvoguti@gmail.com>
2024-09-02 15:57:15 +0200

    retrieving articles

--- scrapers.js
+++ scrapers.js
@@ -5,26 +5,54 @@ async function scrapeProduct(url) {
   const page = await browser.newPage();
   await page.goto(url);
 
-  const allArticles = await page.evaluate(() => {
-    const articles = document.querySelectorAll("tr.athing");
-    return Array.from(articles)
-      .slice(0, 4)
-      .map((article) => {
-        const rank = article.querySelector(".rank").innerText;
-        const title = article.querySelector(".title").innerText;
-        const titleline = article.querySelector(".titleline").innerText;
-        const score = article.querySelector(".score").innerText;
-        // number of comments ?
-        return { rank, title, titleline };
-      });
+  const articles = await page.evaluate(() => {
+    const rows = Array.from(document.querySelectorAll("tr"));
 
-    const subLines = document.querySelectorAll("td.subtext");
+    const articles = [];
+    let currentArticle = null;
+
+    rows.map((row) => {
+      if (row.classList.contains("athing")) {
+        const titleLine = row.querySelector("span.titleline");
+
+        const rankLine = row.querySelector("span.rank");
+        const title = titleLine ? titleLine.innerText : null;
+        const rank = rankLine ? rankLine.innerText : null;
+
+        if (currentArticle) {
+          articles.push(currentArticle);
+        }
+
+        // new article object
+        currentArticle = { rank, title, score: null, comments: null };
+      } else if (currentArticle) {
+        const scoreLine = row.querySelector("span.score");
+        const score = scoreLine ? scoreLine.innerText : null;
+
+        const commentsElement = Array.from(row.querySelectorAll("a")).find(
+          (a) => a.innerText.includes("comment")
+        );
+
+        const comments = commentsElement ? commentsElement.innerText : null;
+
+        currentArticle.comments = comments;
+        currentArticle.score = score;
+
+        articles.push(currentArticle);
+
+        currentArticle = null;
+      }
+    });
+    if (currentArticle) {
+      articles.push(currentArticle);
+    }
+
+    return articles;
   });
 
-  console.log("allArticles :>> ", allArticles);
-  browser.close();
+  console.log("articles :>> ", articles);
+
+  await browser.close();
 }
-//number
-// title
-// points
-// comments
+
+scrapeProduct("https://news.ycombinator.com/");
